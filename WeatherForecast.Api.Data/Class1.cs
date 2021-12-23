8b570ca145408fe174d7ae43dec4d569e251ec6f
Batrak Yurii <navid7498@gmail.com>
2021-12-23 18:05:28 +0200

    Added ClassLibraries for Data And Business Layers

---
+++ WeatherForecast.Api.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace WeatherForecast.Api.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ WeatherForecast.Api.Services/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace WeatherForecast.Api.Services
+{
+    public class Class1
+    {
+    }
+}
